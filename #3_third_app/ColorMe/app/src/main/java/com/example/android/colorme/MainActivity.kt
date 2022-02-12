d0bae36c90427d97cfd797dcb0e61bfeb275bab6
Hashem Alkeshawi <hashemalkeshawi@gmail.com>
2022-02-12 22:05:41 +0200

    colorMyViews from udacity

---
+++ #3_third_app/ColorMe/app/src/main/java/com/example/android/colorme/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.android.colorme
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val v1: View = tex
+    }
+}
